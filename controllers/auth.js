97bf72a99b21f1165e5d39ae35c539a727d4c02a
Sabrina Bosco <sabrina@wonderweblabs.io>
2016-11-24 17:04:06 +0100

    fix user save registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,36 +3,35 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 
 module.exports = {
+    register: function (req, res) {
+        console.log(req.body);
 
-  register: function(req, res){
+        User.findOne({
+            email: req.body.email
+        }, function (err, existingUser) {
 
-    User.findOne({
-      email: req.body.email
-    }, function (err, existingUser) {
+            if(existingUser)
+                return res.status(409).send({message: 'Email is already registered'});
 
-      if(existingUser)
-        return res.status(409).send({message: 'Email already registered!'});
+            var user = new User(req.body);
 
-      var user = new User(req.body);
-
-      user.save(function(err, result){
-        if(err) {
-          res.status(500).send({message: err.message});
-        }
-        res.status(200).send({token: createToken(result)});
-      });
-
-    });
-
-  }
-
+            user.save(function (err, result) {
+                if (err) {
+                    res.status(500).send({
+                        message: err.message
+                    });
+                }
+                res.status(200).send({token: createToken(result)});
+            })
+        });
+    }
 }
 
-function createToken(user) {
-  var payload = {
-    sub: user._id,
-    iat: moment().unix(),
-    exp: moment.add(14, 'days').unix()
-  };
-  return jwt.encode(payload, 'secret');
+function createToken(user){
+    var payload = {
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(14, 'days').unix()
+    };
+    return jwt.encode(payload,'secret');
 }
